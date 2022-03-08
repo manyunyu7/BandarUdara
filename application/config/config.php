b220d2a655d03937c138ce5471296de07eabc2c6
henryaugusta <henryaugusta4@gmail.com>
2022-03-08 10:14:57 +0700

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/BandarUdara/';
+$config['base_url'] = 'http://feylabs.my.id/BandarUdara/';
 
 /*
 |--------------------------------------------------------------------------
